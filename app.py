14f655b37f16b4116d7e9c06afb07731ca10bce7
jpeconomia <69410251+jpeconomia@users.noreply.github.com>
2024-09-02 19:31:29 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import dash
+from dash import Dash, html, dcc
+import pandas as pd 
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
+
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+
+df = pd.read_csv(r"C:\Users\joaop\Documents\Dados de Conjuntura\Dados Estudo\Novo Estudo\investimento e custeio ano safra.csv")
+
+app = Dash(__name__, use_pages=True,external_stylesheets = [dbc.themes.CYBORG, dbc_css])
+load_figure_template('CYBORG')
+
+app.layout = html.Div([
+    html.H1('Crédito acessado por programas de política pública'),
+    html.Div([
+        html.Div(
+            dcc.Link(f"{page['name']}", href=page["relative_path"])
+        ) for page in dash.page_registry.values()
+    ]),
+    dash.page_container
+])
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 8090)
+
