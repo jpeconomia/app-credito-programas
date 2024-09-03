0de3d0f1d350982d0ce3defacbbbb2b4456f5873
jpeconomia <69410251+jpeconomia@users.noreply.github.com>
2024-09-03 11:43:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import dash
 from dash import Dash, html, dcc
 import pandas as pd 
 import dash_bootstrap_components as dbc
-from dash_bootstrap_templates import load_figure_template
+# from dash_bootstrap_templates import load_figure_template
 
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 
 df = pd.read_csv('https://drive.google.com/uc?export=download&id=1_Q4I9iLjxMnRqLzcOAPfTSGsN8F3kRBE')
 
 app = Dash(__name__, use_pages=True,external_stylesheets = [dbc.themes.CYBORG, dbc_css])
-load_figure_template('CYBORG')
+# load_figure_template('CYBORG')
 
 app.layout = html.Div([
     html.H1('Crédito acessado por programas de política pública'),
